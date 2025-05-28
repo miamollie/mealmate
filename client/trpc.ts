1acaa8cdf94e56537d36f1a1818aab69313cee73
miamollie <mia.deburca@vista.com>
2025-05-28 21:59:05 +1000

    Fixup auth session needs to run only in browser not during build

--- client/trpc.ts
+++ client/trpc.ts
@@ -1,6 +1,6 @@
 // Important: By using import type you ensure that the reference will be stripped at compile-time, meaning you don't inadvertently import server-side code into your client.
-
 import type { AppRouter } from "../backend/transport/routers";
+
 import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
 import { authClient } from "./src/auth";
 
@@ -9,7 +9,7 @@ import { createTRPCClient, httpBatchLink } from "@trpc/client";
 export const client = createTRPCClient<AppRouter>({
   links: [
     httpBatchLink({
-      url:  "http://localhost:4000/trpc",
+      url: "http://localhost:4000/trpc", //todo env variable
       headers: async () => {
         const authHeaders = await getAuth();
         return {
@@ -23,7 +23,10 @@ export const client = createTRPCClient<AppRouter>({
 export type RouterInput = inferRouterInputs<AppRouter>;
 export type RouterOutput = inferRouterOutputs<AppRouter>;
 
+// TODO ensure this doesn't execute during SSR or build or it will be borked because authclient can't execute in those contexts
+
 async function getAuth() {
+  if (typeof Window === "undefined") return {};
   const session = await authClient.auth.getSession();
   const token = session.data.session?.access_token;
   return token ? { Authorization: `Bearer ${token}` } : {};
